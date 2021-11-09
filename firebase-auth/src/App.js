f10d3f9ccddde4799a7b19456da41c4d1d7b42c0
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-11-09 13:07:16 +1100

    React Router Version Six Updated

--- firebase-auth/src/App.js
+++ firebase-auth/src/App.js
@@ -1,14 +1,22 @@
-import React from 'react'
-import Form from './Components/Form'
-import Header from './Components/Header'
-import "./CSS/style.css"
+import React from "react";
+import Form from "./Components/Form";
+import Header from "./Components/Header";
+import "./CSS/style.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Error from "./Components/Error";
 const App = () => {
-    return (
-        <div>
-            <Header/>
-          <Form/>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Form/>}/>
+          <Route path="*" element={<Error/>}/>
+        
+        </Routes>
+      </Router>
+    </div>
+  );
+};
 
-export default App
+export default App;
