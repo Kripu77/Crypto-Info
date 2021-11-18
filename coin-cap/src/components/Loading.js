d7dd84cdc03d776019964f65c4fc385a652f568e
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-11-18 15:27:09 +1100

    Pagination Component Ready

--- coin-cap/src/components/Loading.js
+++ coin-cap/src/components/Loading.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Loading = () => {
     return (
-      <div className="flex items-center justify-center mt-52">
+      <div className="flex items-center justify-center mt-80">
         <div className="w-40 h-40 border-t-4 border-b-4 border-purple-900 rounded-full animate-spin"></div>
       </div>
     );
